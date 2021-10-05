182189f8eff1d21041247a354f1bc8eb14197f0c
RomanBurgos <romanvictor.burgos@benilde.edu.ph>
2021-10-05 15:22:56 +0800

    Added Sample Project

---
+++ Sample Project/src/com/romanburgos/Main.java
@@ -0,0 +1,10 @@
+package com.romanburgos;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //print hello
+        System.out.println("I am the Bone of my Sword");
+    }
+}
