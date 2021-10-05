7216d6cd8feaaf5407030219b3467cd2e17f865b
RomanBurgos <romanvictor.burgos@benilde.edu.ph>
2021-10-05 15:44:58 +0800

    Added a sample comment

---
+++ Class/src/com/romanburgos/Main.java
@@ -0,0 +1,20 @@
+package com.romanburgos;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //hello
+
+        //Get user output
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hi whats your name? ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+
+    }
+}
