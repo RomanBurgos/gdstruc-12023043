48b6f6dff23a60edd2b28d92bb1deedb0f9ad267
RomanBurgos <romanvictor.burgos@benilde.edu.ph>
2021-10-05 15:32:40 +0800

    Added a Sample change comment

--- Sample/src/com/romanburgos/Main.java
+++ Sample/src/com/romanburgos/Main.java
@@ -3,8 +3,6 @@ package com.romanburgos;
 public class Main {
 
     public static void main(String[] args) {
-
-        //print hello
-        System.out.println("I am the Bone of my Sword");
+	// write your code here
     }
 }
