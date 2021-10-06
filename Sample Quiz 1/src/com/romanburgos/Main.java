0d42903ac1bb983b0155196e499cb8375ae11e6d
RomanBurgos <romanvictor.burgos@benilde.edu.ph>
2021-10-06 09:07:52 +0800

    Added Practice Files

--- Sample Quiz 1/src/com/romanburgos/Main.java
+++ Sample Quiz 1/src/com/romanburgos/Main.java
@@ -3,6 +3,30 @@ package com.romanburgos;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int[] numbers = new int[20];
+
+        numbers[0] = 90;
+        numbers[1] = 65;
+        numbers[2] = 15;
+        numbers[3] = 50;
+        numbers[4] = 105;
+        numbers[5] = 90;
+        numbers[6] = 65;
+        numbers[7] = 15;
+        numbers[3] = 50;
+        numbers[4] = 105;
+
+        int index =-1;
+
+        for(int i = 0; i < numbers.length; i++)
+        {
+            if (numbers[i] == 50)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        System.out.println(index);
     }
 }
